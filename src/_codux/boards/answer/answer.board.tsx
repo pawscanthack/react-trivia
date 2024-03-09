eb636e2ceefbc61907656abe6a61c0c31f6caccc
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2024-03-09 00:46:25 -0500

    Removed _codux folder

--- src/_codux/boards/answer/answer.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import Answer from '../../../components/Answer';
-
-export default createBoard({
-    name: 'AnswerRed',
-    Board: () => <Answer text="Dick" color="red" />,
-    isSnippet: true,
-});
--- src/_codux/boards/question/question-comp.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import QuestionComp from '../../../components/Question';
-
-export default createBoard({
-    name: 'QuestionComp',
-    Board: () => <QuestionComp />,
-    isSnippet: true,
-});
